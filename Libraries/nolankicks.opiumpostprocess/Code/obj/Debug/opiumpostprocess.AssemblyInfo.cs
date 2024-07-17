87df823f80de99f340cc3725665786d5cda0a317
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-07-17 20:07:15 +1000

    Added shop, cleaned up some UI

--- Libraries/nolankicks.opiumpostprocess/Code/obj/Debug/opiumpostprocess.AssemblyInfo.cs
+++ Libraries/nolankicks.opiumpostprocess/Code/obj/Debug/opiumpostprocess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("opiumpostprocess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40452bfdb8bbbff55fa15727bd3a6e9d97d53c92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10960f1310a064fb78d93a9821cb78f2d6002ede")]
 [assembly: System.Reflection.AssemblyProductAttribute("opiumpostprocess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("opiumpostprocess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
